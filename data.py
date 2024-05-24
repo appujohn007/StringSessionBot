b58fba68b7fb34add31c7ce50cb187e65b14db64
Nioboy007 <161853537+Nioboy007@users.noreply.github.com>
2024-05-24 14:21:08 +0530

    Update data.py

--- data.py
+++ data.py
@@ -13,31 +13,31 @@ class Data:
 
     buttons = [
         generate_single_button,
-        [InlineKeyboardButton("✨ Bot Status and More Bots ✨", url="https://t.me/StarkBots/7")],
         [
             InlineKeyboardButton("How to Use ❔", callback_data="help"),
             InlineKeyboardButton("🎪 About 🎪", callback_data="about")
         ],
-        [InlineKeyboardButton("♥ More Amazing bots ♥", url="https://t.me/StarkBots")],
+        [InlineKeyboardButton("♥ More Amazing bots ♥", url="https://t.me/botio_devs")],
     ]
 
     START = """
-Hey {}
+Hello 👋 {}
 
-Welcome to {}
+Welcome to 🙃{}
 
-If you don't trust this bot, 
-1) stop reading this message
-2) delete this chat
-
-Still reading?
 You can use me to generate pyrogram (even version 2) and telethon string session. Use below buttons to learn more !
 
-By @StarkBots
+This is a 100% secure bot which makes string session utterly encrypted
+ - No strings are logged 🔒 *many bots log their users sessions to hack those accounts 😵
+ - 100% legit
+ - 24/7 support 
+ 
+By [Io Devs](https://t.me/botio_devs)
     """
 
     HELP = """
-✨ **Available Commands** ✨
+✨ 
+**Available Commands** ✨
 
 /about - About The Bot
 /help - This Message
@@ -52,11 +52,9 @@ By @StarkBots
 
 Telegram Bot to generate Pyrogram and Telethon string session by @StarkBots
 
-Source Code : [Click Here](https://github.com/StarkBotsIndustries/StringSessionBot)
-
 Framework : [Pyrogram](https://docs.pyrogram.org)
 
 Language : [Python](https://www.python.org)
 
-Developer : @StarkAgent
+Developer : [Appuz](https://t.me/Appuz_007)
     """
